bd6046a6bdb4c3708a3410227ad3f91520fba49b
dpwhite2 <dpwhite2@opus-dev.cnl.ncsu.edu>
2010-08-25 17:17:05 -0400

    Added a rudimentary CSV exporter for the WorkItem view in the admin tool.

--- admin.py
+++ admin.py
@@ -1,12 +1,53 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 
+from django.http import HttpResponse
+import csv
+
 from worklog.models import WorkItem, Job, WorkLogReminder
 #import worklog.admin_filter
+
+def export_csv(modeladmin, request, queryset):
+    pass
+export_csv.short_description = "Save the selected Work Items to a CSV file."
 
 class WorkItemAdmin(admin.ModelAdmin):
     list_display = ('user','date','hours','text','job')
     list_filter = ('user','date','job')
+    
+    def changelist_view(self, request, extra_context=None):
+        if 'export_as_csv' in request.POST:
+            response = HttpResponse(mimetype='text/csv')
+            response['Content-Disposition'] = 'attachment; filename=somefilename.csv'
+            
+            ChangeList = self.get_changelist(request)
+            
+            cl = ChangeList(request, self.model, self.list_display, self.list_display_links, self.list_filter,
+                self.date_hierarchy, self.search_fields, self.list_select_related, self.list_per_page, self.list_editable, self) 
+                
+            rows = []
+            for item in cl.query_set:
+                row = list()
+                row.append(str(item.user.pk))
+                # if no first/last name available, fall back to username
+                if item.user.last_name:
+                    row.append('{0} {1}'.format(item.user.first_name,item.user.last_name))
+                else:
+                    row.append(item.user.username)
+                row.append(item.job.name)
+                row.append(str(item.date))
+                row.append(str(item.hours))
+                row.append(item.text)
+                rows.append(row)
+            
+            writer = csv.writer(response)
+            for row in rows:
+                writer.writerow(row)
+
+            return response
+
+        else:
+            return super(WorkItemAdmin,self).changelist_view(request, extra_context)
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('name','open_date','close_date')
--- views.py
+++ views.py
@@ -69,39 +69,11 @@ def createWorkItem(request, reminder_id=None):
             {'form': form, 'reminder_id': reminder_id},
                             context_instance=RequestContext(request))
 
-# @login_required
-# def createWorkItemReminder(request, reminder_id):    
-    # rems = None
-    # if not reminder_id:
-        # return HttpResponse('Expected a reminder id.')
-    # rems = WorkLogReminder.objects.filter(reminder_id=reminder_id)
-    # if not rems:
-        # return HttpResponse(no_reminder_msg)
-    # if request.user != rems[0].user:
-        # return HttpResponse('The current user name does not match the user saved with the given id.')
-    # date = rems[0].date
-    # actsuffix = 'reminder_%s/'%(reminder_id,)
+@login_required
+def viewWorkItemCSV(request):
+    #if 
+    pass
     
-    # if request.method == 'POST': # If the form has been submitted...
-        # #item = WorkItem(user=request.user, date=date)
-        # form = WorkItemForm(request.POST, available_jobs=Job.get_jobs_open_on(date), reminder_id=reminder_id)
-        # if form.is_valid(): # All validation rules pass
-            # # Process the data in form.cleaned_data
-            # hours = form.cleaned_data['hours']
-            # text = form.cleaned_data['text']
-            # job = form.cleaned_data['job']
-            # wi = WorkItem(user=request.user, date=date, hours=hours, text=text, job=job)
-            # wi.save()
-            # # When a user submits a form with a reminder_id, delete the record of the reminder_id
-            # #rems[0].delete()
-            # return HttpResponseRedirect('/worklog/view/%s/%s/' % (request.user.username, str(date))) # Redirect after POST
-    # else:
-        # form = WorkItemForm(available_jobs=Job.get_jobs_open_on(date), reminder_id=None) # An unbound form
-
-    # #return HttpResponse('form = %s' % form)
-    # return render_to_response('worklog/workform.html',
-                              # {'form': form, 'actsuffix': actsuffix},
-                              # context_instance=RequestContext(request))
 '''
 
 def thanks(request):
