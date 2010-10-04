950c5139f7f80e58876c28548882818c7ffa12ce
David White <dpwhite2@ncsu.edu>
2010-10-04 09:34:43 -0400

    Changed to new directory structure to meet Hudson test needs.

---
+++ test_settings.py
@@ -0,0 +1,9 @@
+from settings import *
+
+BROKER_HOST =     'localhost'
+BROKER_PORT =     5672
+BROKER_USER =     'dpwhite2_user'
+BROKER_PASSWORD = 'Fraz8pHAy6sep3Et'
+BROKER_VHOST =    'dpwhite2_vhost'
+
+WORKLOG_REMINDER_EMAIL_LINK_URLBASE = 'http://testserver'
---
+++ worklog/urls.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django.conf.urls.defaults import *
+
+DATEMIN = r'(?P<datemin>\d{4}-\d{2}-\d{2})'
+DATEMAX = r'(?P<datemax>\d{4}-\d{2}-\d{2})'
+# accepts:  date_date   or   date_   or   _date
+DATERANGE1 = '(?:'+DATEMIN+'_'+DATEMAX+'?)'
+DATERANGE2 = '(?:_'+DATEMAX+')'
+
+USERNAME = r'(?P<username>[a-zA-Z0-9]+)'
+##JOBID = r'(?:_job_(?P<jobid>[0-9]+))'
+
+urlpatterns = patterns('worklog',
+    (r'^add/$', 'views.createWorkItem', {'reminder_id': None}),
+    (r'^add/reminder_(?P<reminder_id>[0-9a-f\-]{36})/$','views.createWorkItem', {}, 'worklog-reminder-view'), # last item is the view-name
+    
+    (r'^view/$', 'views.viewWork'),
+    #(r'^view/today/$', 'views.viewWork', {'datemin': datetime.date.today(), 'datemax': datetime.date.today()}),
+    (r'^view/today/$', 'views.viewWork', {'datemin': 'today', 'datemax': 'today'}),
+    (r'^view/'+DATERANGE1+'/$', 'views.viewWork'),
+    (r'^view/'+DATERANGE2+'/$', 'views.viewWork'),
+    (r'^view/'+USERNAME+'/$', 'views.viewWork'),
+    #(r'^view/'+USERNAME+'/today/$', 'views.viewWork', {'datemin': datetime.date.today(), 'datemax': datetime.date.today()}),
+    (r'^view/'+USERNAME+'/today/$', 'views.viewWork', {'datemin': 'today', 'datemax': 'today'}),
+    (r'^view/'+USERNAME+'/'+DATERANGE1+'/$', 'views.viewWork'),
+    (r'^view/'+USERNAME+'/'+DATERANGE2+'/$', 'views.viewWork'),
+)
