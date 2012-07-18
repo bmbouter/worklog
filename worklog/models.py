744872ab35d7eca1007db74c0af39b2e87853687
Tomer Shvueli <tomer@shvueli.com>
2012-07-18 14:25:08 -0400

    Project number and object code are now optional under Biweekly employees.

--- worklog/models.py
+++ worklog/models.py
@@ -7,8 +7,8 @@ import worklog.admin_filter
 class BiweeklyEmployee(models.Model):
     user = models.ForeignKey(User)
     univ_id = models.CharField(max_length=9, verbose_name='University ID')
-    project_num = models.CharField(max_length=255, verbose_name='Project #')
-    obj_code = models.CharField(max_length=255, verbose_name='Obj Code')
+    project_num = models.CharField(max_length=255, blank=True, verbose_name='Project #')
+    obj_code = models.CharField(max_length=255, blank=True, verbose_name='Obj Code')
     hourly_pay = models.DecimalField(max_digits=5, decimal_places=2)
 
     def get_timesheet_name(self):
