d3028a88df3f23307e9022a107fb45626918e345
maheshl <mlagada@ncsu.edu>
2012-10-19 19:14:10 -0400

    Migrations for associating Users with Job Model

---
+++ worklog/migrations/0012_auto__add_field_job_available_all_users.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Job.available_all_users'
+        db.add_column('worklog_job', 'available_all_users',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+        # Adding M2M table for field users on 'Job'
+        db.create_table('worklog_job_users', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('job', models.ForeignKey(orm['worklog.job'], null=False)),
+            ('user', models.ForeignKey(orm['auth.user'], null=False))
+        ))
+        db.create_unique('worklog_job_users', ['job_id', 'user_id'])
+
+
+    def backwards(self, orm):
+        # Deleting field 'Job.available_all_users'
+        db.delete_column('worklog_job', 'available_all_users')
+
+        # Removing M2M table for field users on 'Job'
+        db.delete_table('worklog_job_users')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'worklog.billingschedule': {
+            'Meta': {'object_name': 'BillingSchedule'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'billing_schedule'", 'to': "orm['worklog.Job']"})
+        },
+        'worklog.biweeklyemployee': {
+            'Meta': {'object_name': 'BiweeklyEmployee'},
+            'hourly_pay': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'obj_code': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'project_num': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'univ_id': ('django.db.models.fields.CharField', [], {'max_length': '9'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'worklog.funding': {
+            'Meta': {'object_name': 'Funding'},
+            'date_available': ('django.db.models.fields.DateField', [], {}),
+            'hours': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'funding'", 'to': "orm['worklog.Job']"})
+        },
+        'worklog.holiday': {
+            'Meta': {'object_name': 'Holiday'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {})
+        },
+        'worklog.job': {
+            'Meta': {'object_name': 'Job'},
+            'available_all_users': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'close_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'do_not_invoice': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'open_date': ('django.db.models.fields.DateField', [], {}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'worklog.workitem': {
+            'Meta': {'object_name': 'WorkItem'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'do_not_invoice': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hours': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoiced': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'job': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['worklog.Job']"}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'worklog.worklogreminder': {
+            'Meta': {'object_name': 'WorkLogReminder'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'reminder_id': ('django.db.models.fields.CharField', [], {'max_length': '36'}),
+            'submitted_jobs': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['worklog.Job']", 'symmetrical': 'False'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'worklog.workperiod': {
+            'Meta': {'object_name': 'WorkPeriod'},
+            'due_date': ('django.db.models.fields.DateField', [], {}),
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pay_day': ('django.db.models.fields.DateField', [], {}),
+            'payroll_id': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'start_date': ('django.db.models.fields.DateField', [], {})
+        }
+    }
+
+    complete_apps = ['worklog']
